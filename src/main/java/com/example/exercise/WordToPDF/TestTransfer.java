bbbe3ae0e69bb69f258116157f54913721e70c90
Jevua Xu <1178280528@qq.com>
2018-10-12 23:18:47 +0800

    add WordToPDF

--- src/main/java/com/example/exercise/WordToPDF/TestTransfer.java
+++ src/main/java/com/example/exercise/WordToPDF/TestTransfer.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class TestTransfer {
+    //官网：www.xdocin.com
     public static void main(String[] args) {
         XDocService service = new XDocService();
         try {
